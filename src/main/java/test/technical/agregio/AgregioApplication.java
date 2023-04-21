1649b841bf87b4d06d3cfdd8e7b44fd30e89fa72
Arthur Serre <arthur.serre@gmail.com>
2023-04-21 11:40:38 +0200

    initializing project

---
+++ src/main/java/test/technical/agregio/AgregioApplication.java
@@ -0,0 +1,13 @@
+package test.technical.agregio;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AgregioApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(AgregioApplication.class, args);
+    }
+
+}
---
+++ src/test/java/test/technical/agregio/AgregioApplicationTests.java
@@ -0,0 +1,13 @@
+package test.technical.agregio;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AgregioApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
